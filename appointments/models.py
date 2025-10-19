1130136e6195dc063aeeb46e986e4a48110cd485
NoluvoBinza <noluvobinza@gmail.com>
2025-10-19 16:41:31 +0200

    all staged files

--- appointments/models.py
+++ appointments/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Doctor(models.Model):
+    name = models.CharField(max_length=100)
+    specialization = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class Patient(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    contact = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+
+class Appointment(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    reason = models.TextField()
+    
+
+    def __str__(self):
+        return f"{self.Patient.name} - {self.date} {self.time}"
